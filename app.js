89cdcf75fa6c40c2a2af78aea077f514fae7c369
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-09-01 01:12:22 +0530

    chore: Update CORS configuration to allow specific origin and methods

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ import { testmail } from "./src/controllers/sendmail.controller.js";
 const app = express();
 
 dotenv.config();
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
