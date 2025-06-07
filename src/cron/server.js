194fb5e7d98a7fc21513d6bec6e4371aa28ba2c4
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-06-07 18:21:59 +0530

    fix: add cron job to run verification process daily at 6:30 PM

--- src/cron/server.js
+++ src/cron/server.js
@@ -12,6 +12,9 @@ import {
 
 cron.schedule("0 */5 * * *", runVerificationJob); // every 5 hours
 
+// run at 18:30 every day
+cron.schedule("30 18 * * *", runVerificationJob); // every day at 6:30 PM
+
 async function runVerificationJob() {
     console.log("⏳ Running payment verification retry job...");
     sendMobileLog("Payment verification retry job started.");
